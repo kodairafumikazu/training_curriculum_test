e10c030bd6a8ce573369a2ce1367fffa7dcc4d21
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2020-12-23 19:55:54 +0900

    lssue4〜６の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,9 +19,8 @@ class CalendarsController < ApplicationController
   end
 
 
-  def getWeek
-    require "date"
-    day = Date.today.wday
+  def get_week
+    
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
@@ -29,7 +28,7 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @week_days.push(days)
+    @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
     
@@ -40,17 +39,21 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
+      
 
-      wday_num = # wdayメソッドを用いて取得した数値
-      if wday_num <= 7
-        puts num
-      else #「wday_numが7以上の場合」という条件式
+      wday_num = @todays_date.wday + x
+
+      if wday_num >= 7
         wday_num = wday_num -7
       end 
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => (@today_wday + x).wday }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num] }
 
       @week_days.push(days)
+
+      
+      
+
     end
 
   end
